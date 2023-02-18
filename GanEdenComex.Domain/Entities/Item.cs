804ec4ff1c19fbf865fbce3433a04b9a234b60e8
Marcelo Cabral <mh.cabral1@gmail.com>
2023-02-18 19:14:35 -0400

    Atualizando tabela Item

--- GanEdenComex.Domain/Entities/Item.cs
+++ GanEdenComex.Domain/Entities/Item.cs
@@ -10,18 +10,41 @@ namespace GanEdenComex.Domain.Entities
     public class Item: BaseEntity
     {
         public string? PartNumber { get; set; }
-        public int? CodigoInterno { get; set; }
-        public string? UnidadeOrganizacional { get; set; }
+        public string? partNumberInterno { get; set; }
         public string? DescricaoDetalhada { get; set;}
-         public string? DescricaoNFE { get; set;}
         [ForeignKey("Fornecedor")]
         public int? IdFornecedor { get; set; }
         public virtual Fornecedor? Fornecedor { get; set; }
         [ForeignKey("NCM")]
         public int? IdNCM { get; set; }
         public virtual NCM? NCM { get; set; }
-        public int? NALADI { get; set; }
-        public string? DescricaoNALADI { get; set; }
+        [ForeignKey("Unidade")]
+        public int? IdUnidade { get; set; }
+        public virtual Unidade? unidade { get; set; }
+        public string? DescricaoNFE { get; set; }
+        public string? detalheNcm { get; set; }
+        public string? ProdutoSuframa { get; set; }
+        public string? DescricaoProdutoSuframa { get; set; }
+        public string? tipoProduto { get; set; }
+        public string? tratamentoAdministrativo { get; set; }
+        public double? vlCra { get; set; }
+        public double? AliquotaII {get; set; }
+        public double? AliquotaIPI { get; set; }
+        public double? AliquotaPIS { get; set; }
+        public double? AliquotaCOFINS { get; set; }
+        public string? UnidadeOrganizacional { get; set; }
+        public DateTime? dtModificacao { get; set; }
+        [ForeignKey("Empresa")]
+        public int? IdEmpresa { get; set; }
+        public virtual Empresa? Empresa { get; set; }
+
+        /* Grande change ficar aqui essa informação
+        [ForeignKey("AcordoAladi")]
+        public int? IdAcordoAladi { get; set; }
+        public virtual AcordoAladi? acordoaladi { get; set; }
+        */
+
+        /*
         public string? CodigoExTarifario { get; set; }
         public string? OrgaoExTarifario { get; set; }
         public string? TipoExTarifario { get; set; }
@@ -32,22 +55,7 @@ namespace GanEdenComex.Domain.Entities
         public string? TipoExTIPI { get; set; }
         public string? AtoExTIPI { get; set; }
         public string? AnoExTIPI { get; set; }
-        public string? Destaque { get; set; }
-        public int? ProdutoSuframa { get; set; }
-        public string? DescricaoProdutoSuframa { get; set; }
-        public string? TipoProdutoSuframa { get; set; }
-        public string? DetalheProdutoSuframa { get; set; }
-        public double? AliquotaII {get; set; }
-        public double? AliquotaIPI { get; set; }
-        public double? AliquotaPIS { get; set; }
-        public double? AliquotaCOFINS { get; set; }
-        [ForeignKey("Empresa")]
-        public int? IdEmpresa { get; set; }
-        public virtual Empresa? Empresa { get; set; }
-        /*
-        [ForeignKey("Fornecedor")]
-        public int? IdFornecedor { get; set; }
-        public virtual Fornecedor? Fornecedor { get; set; }
         */
+       
     }
 }
--- GanEdenComex.Domain/Entities/NCM.cs
+++ GanEdenComex.Domain/Entities/NCM.cs
@@ -13,21 +13,21 @@ namespace GanEdenComex.Domain.Entities
         [ForeignKey("Unidade")]
         public int? IdUnidade { get; set; }
         public virtual Unidade? unidade { get; set; }
-        //public Capitulo capitulo { get; set; }
-        //public Posicao posicao { get; set; }
+        //public Capitulo capitulo { get; set; } //classe desconhecida
+        //public Posicao posicao { get; set; } //classe desconhecida
         public double? vlIi { get; set; }
         public double? vlIiMercosul { get; set; }
         public double? vlIpi { get; set; }
         public double? vlPis { get; set; }
         public double? vlCofins { get; set; }
         public DateTime? dtAtualizacao { get; set; }
-        //public List<SxTratamentoAdministrativo> sxTratamentoAdministrativo { get; set; }
-        //public List<object> sxNve { get; set; }
-        //public List<SxExTarifario> sxExTarifarios { get; set; }
-        //public List<object> detalhes { get; set; }
-        //public object atributos { get; set; }
+        //public List<SxTratamentoAdministrativo> sxTratamentoAdministrativo { get; set; } //lista de tabela auxiliar
+        //public List<object> sxNve { get; set; } //lista auxiliar desconhecida
+        //public List<SxExTarifario> sxExTarifarios { get; set; } //lista de uma tabela auxiliar
+        //public List<object> detalhes { get; set; } //lista auxiliar desconhecida
+        //public object atributos { get; set; } //objeto desconhecido
         public string? nome { get; set; }
         public string? codigo { get; set; }
-        //public ComplementosExportacao complementosExportacao { get; set; }
+        //public ComplementosExportacao complementosExportacao { get; set; } //tabela desconhecida
     }
 }
